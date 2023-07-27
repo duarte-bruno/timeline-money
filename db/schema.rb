a32590d93433abceb63ad8c6838146c5bad59a59
Bruno Duarte Pereira Santos <duartebrunops@gmail.com>
2023-07-27 09:11:00 -0300

    Removed the username attibute from user.

--- db/schema.rb
+++ db/schema.rb
@@ -43,13 +43,11 @@ ActiveRecord::Schema[7.0].define(version: 4) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
-    t.string "username", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "accounts", "users"
